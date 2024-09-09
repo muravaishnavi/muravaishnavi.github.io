110108965e23586b4d86066aaa7545ca25b95fb4
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2024-09-09 11:35:46 +0530

    generate song index

---
+++ server/index.js
@@ -0,0 +1,53 @@
+import fs  from 'fs';
+import { readdir } from 'fs/promises';
+import path from 'path';
+import async from 'async';
+
+const GetSongIndex = (fileNames) => {
+  return new Promise((resolve, reject) => {
+    async.map(fileNames, fs.readFile, (err, data) => {
+      if (err) {
+        console.error('Error reading file:');
+        return reject(err);
+      }
+      const songIndex = data.map((song) => {
+        const parsedJson = JSON.parse(song);
+        return {
+          id: parsedJson.id,
+          title: parsedJson.title,
+          genre: parsedJson.artist,
+          album: parsedJson.album,
+          composer: parsedJson.year,
+          tag: parsedJson.tag,
+          }
+      });
+      resolve(songIndex);
+    });
+  });
+}
+
+
+// Usage example
+const directoryPath = '../data';
+const dirFiles = await readdir(directoryPath, {recursive: true, withFileTypes: true,encoding: 'utf8'}); 
+// Exclude index.json and schema.json
+const excludeFiles = ['index.json', 'schema.json'];
+const fileNames = dirFiles.filter(file => file.name.endsWith('.json') && !excludeFiles.includes(file.name)).map(file => `${file.path}\\${file.name}`);
+
+
+GetSongIndex(fileNames).then((songIndex) => {
+  fs.writeFile('../data/index.json', JSON.stringify(songIndex), (err) => {
+    if (err) {
+      console.error('Error writing file:');
+      console.error(err);
+      return;
+    }
+    console.log('Finished writing file');
+  });
+}).catch((err) => {
+  console.error(err);
+});
+
+
+
+
---
+++ server/test.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+
+var stream = fs.createWriteStream("append.txt", {flags:'a'});
+console.log(new Date().toISOString());
+[...Array(10000)].forEach( function (item,index) {
+    stream.write(index + "\n");
+});
+console.log(new Date().toISOString());
+stream.end();
